5b6fd49b7d0a9992bf8a94351302be24a096605c
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-22 20:36:11 +0530

    Created Header which includes navigationbar

---
+++ partials/__header.php
@@ -0,0 +1,53 @@
+<?php 
+
+session_start();
+require '__dbconnect.php';
+
+?>
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary position-sticky top-0 z-3" data-bs-theme="dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">iForum</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="contact.php">Contact</a>
+                </li>
+
+            </ul>
+            <form class="d-flex me-2" role="search" method="get" action="search.php">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search" id="search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+
+            <?php 
+            
+            if(isset($_SESSION['loggedIn']) AND $_SESSION['loggedIn'] == 'yes')
+            {
+                $name = $_SESSION['name'];
+                echo '<div class="my-2 d-flex justify-content-start align-items-center">
+                    <span class="text-light me-2">'.$name.'</span>
+                    <a href="partials/__logout.php" class="btn btn-outline-danger text-light">Logout</a>
+                    </div>';
+            }
+            else
+            {
+                echo '<div class="my-2">
+                    <a href="login.php" class="btn btn-outline-primary text-light">Login</a>
+                    <a href="signup.php" class="btn btn-outline-primary text-light">Signup</a>
+                    </div>';
+            }
+            
+            ?>
+
+        </div>
+    </div>
+</nav>
