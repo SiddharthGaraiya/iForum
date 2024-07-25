01bf3fbcf58a8bbf817fb9f7f640ba6f6a37e806
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-25 11:07:13 +0530

    Create not_found.php | not found page

---
+++ not_found.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>iForum - Not Found</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        a:hover {
+            text-decoration: underline !important;
+        }
+    </style>
+</head>
+
+<body class="d-flex justify-content-center align-items-center flex-column" style="min-height: 100vh">
+    <h1>Invalid URL, check the URL and try again.</h1>
+    <h2><a href="index.php" class="text-decoration-none">Go to Homepage</a></h2>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
