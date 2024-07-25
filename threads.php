b1ae529efcea9aa544a78fed1e972258c10a280f
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-25 11:15:15 +0530

    Update threads.php | displayed all threads related selected catagory

--- threads.php
+++ threads.php
@@ -86,4 +86,55 @@
             </div>
         </div>
 
-     
+        <div class="p-4 my-5 bg-body-tertiary rounded-4 border shadow">
+            <div class="container-fluid my-4">
+
+                <h2 class="display-6 fw-semibold pb-3">Browse Threads -</h2>
+                <?php
+
+                $sql_get_threads = "SELECT * FROM `threads` WHERE `category_id` = '$category_id'";
+                $result = mysqli_query($conn, $sql_get_threads);
+
+                if ($result) {
+                    if (mysqli_num_rows($result)) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $thread_description = $row['thread_description'];
+                            if (strlen($thread_description) > 100) {
+                                $thread_description = substr($thread_description, 0, 100);
+                                $thread_description .= '...';
+                            }
+                            $thread_user_id = $row['user_id'];
+                            $sql_get_user = "SELECT * FROM `users` WHERE `user_id` = '$thread_user_id'";
+                            $result_user = mysqli_query($conn, $sql_get_user);
+                            if ($result_user) {
+                                if (mysqli_num_rows($result_user)) {
+                                    while ($row_user = mysqli_fetch_assoc($result_user)) {
+                                        $thread_username = $row_user['username'];
+                                        $timestamp = $row['timestamp'];
+                                        $timestamp = date("h:i a | d-m-Y", strtotime($timestamp));
+
+                                        echo '<div class="d-flex align-items-center my-3">
+                                        <div class="flex-shrink-0" style="width: 7vw">
+                                        <img src="img/forum_user_default.png" alt="" class="img-fluid">
+                                        </div>
+                                        <div class="flex-grow-1 ms-3">
+                                        <p class="my-2 fs-5 fw-semibold m-0"><a href="thread_comments.php?thread_id=' . $row['thread_id'] . '" class="text-decoration-none">' . $row['thread_title'] . '</a> - ' . $thread_username . ' at ' . $timestamp . '
+                                        </p>
+                                        <p class="fs-5">' . $thread_description . '</p>
+                                        </div>
+                                        </div>
+                                        <p class="text-center m-0 opacity-50">────</p>';
+                                    }
+                                }
+                            }
+
+                        }
+                    } else {
+                        echo '<p class="fs-4 my-4"> - No Thread Found, Be the first one to start a thread for this category.</p>';
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
