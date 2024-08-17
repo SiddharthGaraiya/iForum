03a783cfb300ecac4c51ffffe386c4721aba8954
SiddharthGaraiya <parthivshekhliya@gmail.com>
2024-08-17 13:28:51 +0530

    added username validator and password matcher functionalities

--- partials/__footer.php
+++ partials/__footer.php
@@ -8,6 +8,47 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
 
+        <script>
+        let password = document.getElementById('password');
+        let cpassword = document.getElementById('cpassword');
+        let passwordcheck = document.getElementById('passwordCheck');
+        let btn = document.getElementById('signUp');
+
+        cpassword.addEventListener('keyup', () => {
+            if (cpassword.value === password.value) {
+                cpassword.style.borderColor = '#dee2e6';
+                passwordcheck.style.color = 'green';
+                passwordcheck.innerText = "Password Matches";
+                btn.classList.remove('disabled');
+            }
+            else {
+                cpassword.style.borderColor = 'red';
+                passwordcheck.style.color = 'red';
+                document.getElementById('passwordCheck').innerText = "Password not same, please check."
+                btn.classList.add('disabled');
+            }
+        })
+    </script>
+    <script>
+        $(document).ready(function () {
+            $("#username").keyup(function () {
+                var username = $(this).val().trim();
+                if (username != '') {
+                    $.ajax({
+                        url: 'partials/__username_availability_checker.php',
+                        type: 'post',
+                        data: { username: username },
+                        success: function (response) {
+                            $('#username_availability_response').html(response);
+                        }
+                    });
+                } else {
+                    $("#username_availability_response").html("");
+                }
+            });
+        });
+    </script>
+
 </body>
 
 </html>
